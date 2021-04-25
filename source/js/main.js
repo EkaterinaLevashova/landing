3e7ce46a1a60cdc4b5d59f30ab733baa8251076d
Ekaterina Levashova <katrin.levashova@gmail.com>
2021-04-25 16:02:09 +0300

    validation was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,23 @@
 (function() {
-    let openFormButton = document.querySelector('.arrow-down');
 
-    if(openFormButton) {
+    let openFormButton = document.querySelector('.arrow-down');
+    let form = document.querySelector('.form');
+
+    if (openFormButton) {
         openFormButton.addEventListener('click', function(e) {
-            e.preventDefault(); //действие по умолчанию (в данном случае переход нка страницу #)
-            form.open();
+            e.preventDefault(); //действие по умолчанию (в данном случае переход нка страницу #) в случае если форма не сработает
+            MY.form.open();
+        })
+    }
+
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (MY.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
         })
     }
 }());
